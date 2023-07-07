c13432adbdeae97ee3ab0aaa0d1e54767fdcc941
Lea Model <lea.skywalker309@gmail.com>
2023-07-07 18:08:46 +0200

    Get artists data by ID

--- app.js
+++ app.js
@@ -4,6 +4,7 @@ const express = require('express')
 const expressLayouts = require('express-ejs-layouts')
 
 // require spotify-web-api-node package here:
+const SpotifyWebApi = require('spotify-web-api-node')
 
 const app = express()
 
@@ -12,8 +13,44 @@ app.set('view engine', 'ejs')
 app.set('views', __dirname + '/views')
 app.use(express.static(__dirname + '/public'))
 
+const bodyParser = require('body-parser')
+app.use(bodyParser.urlencoded({ extended: true }))
+
 // setting the spotify-api goes here:
+const spotifyApi = new SpotifyWebApi({
+    clientId: process.env.CLIENT_ID,
+    clientSecret: process.env.CLIENT_SECRET,
+  })
+  
+  // Retrieve an access token
+  spotifyApi
+    .clientCredentialsGrant()
+    .then(data => spotifyApi.setAccessToken(data.body['access_token']))
+    .catch(error => console.log('Something went wrong when retrieving an access token', error))
 
 // Our routes go here:
+app.get('/', (req, res) => {
+    res.render('index')
+})
+
+app.get('/:artistId', (req, res) => {
+    console.log(req.query.name);
+
+    spotifyApi
+        .searchArtists(req.query.name)
+        .then(data => {
+            console.log('The received data from the API: ', data.body.artists.items)
+            // save data in variables before rendering
+
+            // ----> 'HERE WHAT WE WANT TO DO AFTER RECEIVING THE DATA FROM THE API'
+            res.render("artist-search-results", {artist: data})
+        })
+        .catch(err => console.log('The error while searching artists occurred: ', err))
+})
+
+
+app.get('/artist-search', (req, res) => {
+    res.render('artist-search-results')
+})
 
 app.listen(3000, () => console.log('My Spotify project running on port 3000 🎧 🥁 🎸 🔊'))
